19b8b10fdb9ce86f4a75c2eccc95d8b267e03c69
Ahmad <ahmadhasanmirza@gmail.com>
2018-08-31 21:39:21 +0200

    Added access modifiers

--- ContactsManager/src/Contact.java
+++ ContactsManager/src/Contact.java
@@ -1,8 +1,33 @@
 public class Contact {
     //Fields
+        private int ID=0;
+        private String name;
+        private String email;
+        private String phoneNumber;
 
-        String name;
-        String email;
-        String phoneNumber;
-
+        public void setName(String name){
+                this.name = name;
+                setID();
+        }
+        public void setEmail(String email){
+                this.email=email;
+        }
+        public void setNumber(String phoneNumber){
+                this.phoneNumber=phoneNumber;
+        }
+        private void setID(){
+                ID=ID+1;
+        }
+        public int returnID(){
+                return ID;
+        }
+        public String getName(){
+                return name;
+        }
+        public String getEmail(){
+                return email;
+        }
+        public String getNumber(){
+                return phoneNumber;
+        }
 }
--- ContactsManager/src/Main.java
+++ ContactsManager/src/Main.java
@@ -5,11 +5,19 @@ public class Main {
     public static void main(String [] args){
         ContactsManager myContactManager = new ContactsManager();
         Contact Ahmad = new Contact();
-        Ahmad.name="Ahmad";
-        Ahmad.email="ahmad@ahmad.com";
-        Ahmad.phoneNumber="0900786010";
+        Contact Ahmad2 = new Contact();
+
+        Ahmad.setName("Ahmad");
+        Ahmad.setEmail("ahmad@ahmad.com");
+        Ahmad.setNumber("0900786010");
         myContactManager.addContact(Ahmad);
-        System.out.println("Phone Number : " + myContactManager.searchContact("Ahmad").phoneNumber);
+
+        Ahmad2.setName("Ahmad2");
+        Ahmad2.setEmail("ahmad2@ahmad2.com");
+        Ahmad2.setNumber("0000000000");
+        myContactManager.addContact(Ahmad2);
+
+        System.out.println("Phone Number : " + myContactManager.searchContact("Ahmad").getNumber());
     }
 
 }
