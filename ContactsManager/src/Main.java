5424bea878efbdfefdd55c381a78deadbb480d0e
Ahmad <ahmadhasanmirza@gmail.com>
2018-08-31 21:22:32 +0200

    Lesson1 Finished

--- ContactsManager/src/Main.java
+++ ContactsManager/src/Main.java
@@ -1,6 +1,15 @@
+
+
+
 public class Main {
     public static void main(String [] args){
-        ContactManager myContactManager = new ContactManager();
+        ContactsManager myContactManager = new ContactsManager();
+        Contact Ahmad = new Contact();
+        Ahmad.name="Ahmad";
+        Ahmad.email="ahmad@ahmad.com";
+        Ahmad.phoneNumber="0900786010";
+        myContactManager.addContact(Ahmad);
+        System.out.println("Phone Number : " + myContactManager.searchContact("Ahmad").phoneNumber);
     }
- 
+
 }
